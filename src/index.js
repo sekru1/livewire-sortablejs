9017590e2b80a8cf1b23b8c9bce3a1b5c08af16c
Sebastian Krüger <info@kruegers.pro>
2025-07-16 22:41:38 +0200

    Sortable Group unterstütz verschachtelte, unabhängige Gruppen

--- src/index.js
+++ src/index.js
@@ -83,39 +83,46 @@ window.Livewire?.directive('sortable', ({ el, directive, component }) => {
 });
 
 window.Livewire?.directive('sortable-group', ({ el, directive, component }) => {
-    // Only fire this handler on the "root" group directive.
-    if (! directive.modifiers.includes('item-group')) {
-        return;
-    }
+    if (!directive.modifiers.includes('item-group')) return;
 
+    
     let options = {};
 
     if (el.hasAttribute('wire:sortable-group.options')) {
         options = (new Function(`return ${el.getAttribute('wire:sortable-group.options')};`))();
     }
 
+    const groupName = options.name || el.closest('[wire\\:sortable-group]')?.getAttribute('wire:sortable-group');
+
+    if (!groupName) return;
+
+    // optional: HTML-Attribut setzen zur besseren Zuordnung
+    el.setAttribute('data-group-name', groupName);
+    console.log(groupName);
     el.livewire_sortable = window.Sortable.create(el, {
         sort: true,
         ...options,
-        draggable: '[wire\\:sortable-group\\.item]',
+        draggable: `[wire\\:sortable-group\\.item]`, // Gruppen-spezifisch
         handle: '[wire\\:sortable-group\\.handle]',
-        dataIdAttr: 'wire:sortable-group.item',
+        dataIdAttr: `wire:sortable-group.item`, // Attribut für ID-Ermittlung
         group: {
-            pull: true,
-            put: true,
+            // pull: true,
+            // put: true,
             ...options.group,
-            name: el.closest('[wire\\:sortable-group]').getAttribute('wire:sortable-group'),
+            name: groupName,
         },
         onSort: (evt) => {
+            console.log('onSort', evt.item.getAttribute('wire:sortable-group.item'));
             if (evt.to !== evt.from && el === evt.from) {
                 return;
             }
 
-            let masterEl = el.closest('[wire\\:sortable-group]');
+            let masterEl = el.closest('[wire\\:sortable-group='+groupName+']');
 
-            let groups = Array.from(masterEl.querySelectorAll('[wire\\:sortable-group\\.item-group]')).map((el, index) => {
+            let groups = Array.from(masterEl.querySelectorAll('[wire\\:sortable-group\\.item-group]')).filter((el) => {
+                return el.getAttribute('wire:sortable-group.options')?.includes(`name: '${groupName}'`);
+            }).map((el, index) => {
                 moveEndMorphMarker(el);
-
                 return {
                     order: index + 1,
                     value: el.getAttribute('wire:sortable-group.item-group'),
@@ -127,8 +134,13 @@ window.Livewire?.directive('sortable-group', ({ el, directive, component }) => {
                     }),
                 };
             });
-
-            masterEl.closest('[wire\\:id]').__livewire.$wire.call(masterEl.getAttribute('wire:sortable-group'), groups);
+            let data = {
+                from: evt.from.getAttribute('wire:sortable-group.item-group'),
+                to: evt.to.getAttribute('wire:sortable-group.item-group'),
+                item: evt.item.getAttribute('wire:sortable-group.item'),
+                items: groups
+            };  
+            masterEl.closest('[wire\\:id]').__livewire.$wire.call(masterEl.getAttribute('wire:sortable-group'), data);
         },
     });
 });
